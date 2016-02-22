bfbc07e49625083019d40f0ae0a633ec795f73fa
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-02-22 12:29:38 +0100

    Fixed Hourly Repeat

--- lib/rp_hourly.py
+++ lib/rp_hourly.py
@@ -5,7 +5,7 @@ import datetime
 class HourlyRepeat(RepeatPattern):
     def __init__(self):
         self.last_play_hour = -1
-        
+
     def can_play(self):
         if datetime.datetime.now().minute == 0:
             if self.last_play_hour != datetime.datetime.now().hour:
